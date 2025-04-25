4722cffbd2eebad249a1578f49347d92cdf4d644
Nikita <Nikita@DESKTOP-13IEN5T>
2025-04-26 00:07:35 +0500

    2D sont DONE

--- 2Darrays/main.cpp
+++ 2Darrays/main.cpp
@@ -9,10 +9,54 @@ void main()
 	const int COLS = 4;
 	int arr[ROWS][COLS] =
 	{
-		{ 1, 2, 3 },
-		{ 4, 5, 6 },
-		{ 7, 8, 9 },
+			{ 1, 2, 3 },
+			{ 4, 5, 6 },
+			{ 7, 8, 9 },
 	};
+
+	// заполнение случайными числами:
+
+	for (int i = 0; i < ROWS; i++);
+	{
+		for (int j = 0; j < COLS; j++)
+		{
+			arr[i][j] = rand() % 100;
+		}
+		cout << endl;
+	}
+	//вывод исходного массива на экран:
+	for (int i = 0; i < ROWS; i++)
+	{
+		for (int j = 0; j < COLS; j++)
+		{
+			cout << arr[i][j] << "\t";
+		}
+		cout << endl;
+	}
+	cout << endl;
+
+	// Сортировка массива: 
+	for (int i = 0; i < ROWS; i++)
+	{
+		for (int j = 0; j < COLS; j++)
+		{
+			for (int k = i; k < ROWS; k++)
+			{
+				for (int l = k == i ? j+1 : 0; l < COLS; l++)
+				{
+					//arr[i][j] - выбранный элемент
+					//arr[k][l] - перебираемый ээлемент
+					if (arr[k][l] < arr[i][j])
+					{
+						int buffer = arr[i][j];
+						arr[i][j] = arr[k][l];
+						arr[k][l] = buffer;
+					}
+				}
+			}
+		}
+	}
+	// вывод отсроитрованого массива на экран:
 
 	for (int i = 0; i < ROWS; i++)
 	{
@@ -22,4 +66,5 @@ void main()
 		}
 		cout << endl;
 	}
+	cout << endl;
 }
